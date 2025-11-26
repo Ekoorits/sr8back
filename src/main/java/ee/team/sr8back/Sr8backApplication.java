e9bfb6f077841b0bda6e83ad2f1339272cbe5539
Ekoorits <enrikoorits@gmail.com>
2025-11-26 22:34:04 +0200

    Initial commit

---
+++ src/main/java/ee/team/sr8back/Sr8backApplication.java
@@ -0,0 +1,13 @@
+package ee.team.sr8back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sr8backApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sr8backApplication.class, args);
+	}
+
+}
