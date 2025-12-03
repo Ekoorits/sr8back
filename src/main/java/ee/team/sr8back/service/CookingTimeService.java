3b70423288f0e6db0b1c8434a3ac617c415a810b
Ekoorits <enrikoorits@gmail.com>
2025-12-03 16:27:28 +0200

    Get CookingTimes Implemented

--- src/main/java/ee/team/sr8back/service/CookingTimeService.java
+++ src/main/java/ee/team/sr8back/service/CookingTimeService.java
@@ -1,16 +1,21 @@
 package ee.team.sr8back.service;
 
+import ee.team.sr8back.controller.cookingtime.dto.CookingTimeResponse;
 import ee.team.sr8back.infrastructure.exception.PrimaryKeyNotFoundException;
 import ee.team.sr8back.persistence.cookingtime.CookingTime;
+import ee.team.sr8back.persistence.cookingtime.CookingTimeMapper;
 import ee.team.sr8back.persistence.cookingtime.CookingTimeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CookingTimeService {
 
     private final CookingTimeRepository cookingTimeRepository;
+    private final CookingTimeMapper cookingTimeMapper;
 
     public Integer getCookingTimeMinutesMax(Integer cookingTimeId) {
         return getValidCookingTimeBy(cookingTimeId).getMinutesMax();
@@ -19,5 +24,10 @@ public class CookingTimeService {
     public CookingTime getValidCookingTimeBy(Integer cookingTimeId) {
         return cookingTimeRepository.findById(cookingTimeId)
                 .orElseThrow(() -> new PrimaryKeyNotFoundException("cookingTimeId", cookingTimeId));
+    }
+
+    public List<CookingTimeResponse> findAllCookingTimes() {
+        List<CookingTime> cookingTimes = cookingTimeRepository.findAll();
+        return cookingTimeMapper.toCookingTimeResponses(cookingTimes);
     }
 }
