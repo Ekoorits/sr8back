1ec39bd94d8a1861e625335241ae15fbf473d374
Hristina <hristina.samarina@gmail.com>
2025-12-03 16:08:06 +0200

    difficulties get

---
+++ src/main/java/ee/team/sr8back/controller/difficulty/DifficultyController.java
@@ -0,0 +1,21 @@
+package ee.team.sr8back.controller.difficulty;
+
+import ee.team.sr8back.controller.difficulty.dto.DifficultyResponse;
+import ee.team.sr8back.service.DifficultyService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class DifficultyController {
+    private final DifficultyService difficultyService;
+@GetMapping("/difficulties")
+@Operation(summary = "Tagastab kõik raskusastmed listina")
+public List<DifficultyResponse> findAllDifficultyTypes() {
+    return difficultyService.findAllDifficultyTypes();
+}
+}
