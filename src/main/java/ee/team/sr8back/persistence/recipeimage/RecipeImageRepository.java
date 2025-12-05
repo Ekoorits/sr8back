4a1c958983a6a09ad129630b007fa8282ce1b64a
Ekoorits <enrikoorits@gmail.com>
2025-12-05 15:34:06 +0200

    Add new recipe func.

---
+++ src/main/java/ee/team/sr8back/persistence/recipeimage/RecipeImageRepository.java
@@ -0,0 +1,6 @@
+package ee.team.sr8back.persistence.recipeimage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipeImageRepository extends JpaRepository<RecipeImage, Integer> {
+}
--- src/main/java/ee/team/sr8back/service/RecipeService.java
+++ src/main/java/ee/team/sr8back/service/RecipeService.java
@@ -1,11 +1,16 @@
 package ee.team.sr8back.service;
 
+import ee.team.sr8back.controller.recipe.dto.NewRecipeDetailsRequest;
 import ee.team.sr8back.controller.recipe.dto.RecipeResponse;
+import ee.team.sr8back.infrastructure.util.BytesConverter;
 import ee.team.sr8back.persistence.recipe.Recipe;
 import ee.team.sr8back.persistence.recipe.RecipeMapper;
 import ee.team.sr8back.persistence.recipe.RecipeRepository;
+import ee.team.sr8back.persistence.recipeimage.RecipeImage;
+import ee.team.sr8back.persistence.recipeimage.RecipeImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,10 +22,17 @@ public class RecipeService {
     private final RecipeMapper recipeMapper;
     private final DifficultyService difficultyService;
     private final CookingTimeService cookingTimeService;
+    private final RecipeImageRepository recipeImageRepository;
+
+    @Transactional
+    public void addNewRecipeDetails(NewRecipeDetailsRequest newRecipeDetailsRequest) {
+        Recipe recipe = createAndSaveRecipe(newRecipeDetailsRequest);
+        handleAddRecipeImage(newRecipeDetailsRequest.getImageData(), recipe);
+    }
 
     public List<RecipeResponse> findRecipesBy(String searchParam) {
         List<Recipe> recipes = recipeRepository.findRecipesBy(searchParam);
-        return recipeMapper.toRecipes(recipes);
+        return recipeMapper.toRecipeResponses(recipes);
     }
 
     public List<RecipeResponse> findRecipesBy(Integer mealTypeId, Integer difficultyId, Integer cookingTimeId) {
@@ -28,7 +40,38 @@ public class RecipeService {
         Integer cookingTimeMinutesMax = cookingTimeService.getCookingTimeMinutesMax(cookingTimeId);
 
         List<Recipe> recipes = recipeRepository.findRecipesBy(mealTypeId, difficultyLevelNumber, cookingTimeMinutesMax);
-        return recipeMapper.toRecipes(recipes);
+        return recipeMapper.toRecipeResponses(recipes);
     }
 
+    private Recipe createAndSaveRecipe(NewRecipeDetailsRequest newRecipeRequest) {
+        Recipe recipe = createNewRecipe(newRecipeRequest);
+        recipeRepository.save(recipe);
+        return recipe;
+    }
+
+    private Recipe createNewRecipe(NewRecipeDetailsRequest newRecipeRequest) {
+        return recipeMapper.toRecipe(newRecipeRequest);
+    }
+
+    private void handleAddRecipeImage(String imageData, Recipe recipe){
+        if(hasImage(imageData)) {
+            createAndSaveRecipeImage(imageData, recipe);
+        }
+    }
+
+    private static boolean hasImage(String imageData) {
+        return !imageData.isEmpty();
+    }
+
+    private void createAndSaveRecipeImage(String imageData, Recipe recipe) {
+        RecipeImage recipeImage = createNewRecipeImage(imageData, recipe);
+        recipeImageRepository.save(recipeImage);
+    }
+
+    private RecipeImage createNewRecipeImage(String imageData, Recipe recipe) {
+        RecipeImage recipeImage = new RecipeImage();
+        recipeImage.setImageData(BytesConverter.stringToBytes(imageData));
+        recipeImage.setRecipe(recipe);
+        return recipeImage;
+    }
 }
