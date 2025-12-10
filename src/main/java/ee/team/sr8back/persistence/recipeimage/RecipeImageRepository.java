9b3b2c11697cd74fba5249fca877f13b6dfd675b
ANNIKA <annika.avaste@gmail.com>
2025-12-10 11:23:46 +0200

    fixed merge problems

--- src/main/java/ee/team/sr8back/persistence/recipeimage/RecipeImageRepository.java
+++ src/main/java/ee/team/sr8back/persistence/recipeimage/RecipeImageRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface RecipeImageRepository extends JpaRepository<RecipeImage, Integer> {
+
     @Query("select r from RecipeImage r where r.recipe.id = :recipeId")
     Optional<RecipeImage> findRecipeImageBy(Integer recipeId);
-
 
 }
