8c9fb0b3cfd23d48fc8226c413bebe160539aeb5
Ekoorits <enrikoorits@gmail.com>
2025-12-10 15:36:54 +0200

    RecipeIngredientMapper fixed.

--- src/main/java/ee/team/sr8back/persistence/recipeingredient/RecipeIngredientMapper.java
+++ src/main/java/ee/team/sr8back/persistence/recipeingredient/RecipeIngredientMapper.java
@@ -9,10 +9,11 @@ import java.util.List;
 public interface RecipeIngredientMapper {
 
 
-    @Mapping(source = "ingredient.id", target = "recipeIngredientId")
+    @Mapping(source = "id", target = "recipeIngredientId")
     @Mapping(source = "ingredient.name", target = "recipeIngredientName")
     @Mapping(source = "amount", target = "recipeIngredientAmount")
     @Mapping(source = "ingredient.measureUnit.name", target = "recipeIngredientMeasureUnit")
+    @Mapping(source = "ingredient.description", target = "recipeIngredientDescription")
     RecipeIngredientResponse toRecipeIngredientResponse(RecipeIngredient recipeIngredient);
 
     List<RecipeIngredientResponse> toRecipeIngredientResponses(List<RecipeIngredient> recipeIngredients);
